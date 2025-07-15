a15e1c5a849786ec77579b547c5b1574cb487a92
Rodrigo Pinheiro Matias <rodrigopmatias@gmail.com>
2025-07-15 10:48:28 -0300

    feat: foi particionado o projeto para modularizar a produção de controllers

--- {{ cookiecutter.project_slug }}/{{ cookiecutter.project_module}}/helpers/__init__.py
+++ {{ cookiecutter.project_slug }}/{{ cookiecutter.project_module}}/helpers/__init__.py
@@ -1,7 +1,1 @@
-from typing import AsyncGenerator, TypeVar
 
-T = TypeVar("T")
-
-
-async def to_list(g: AsyncGenerator[T, None]) -> list[T]:
-    return [item async for item in g]
--- {{ cookiecutter.project_slug }}/{{ cookiecutter.project_module}}/helpers/models.py
+++
@@ -1,6 +0,0 @@
-from sqlalchemy import Integer
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
-
-
-class DBModel(DeclarativeBase):
-    id: Mapped[int] = mapped_column(Integer(), primary_key=True, autoincrement=True)
